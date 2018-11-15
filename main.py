19fff233ec91622f3cae6cf59122027cc772d865
Roujia Wang <rosesharpaywang@RoujiamatoMacBook-Pro.local>
2018-11-15 00:08:12 -0500

    Create user class

---
+++ main.py
@@ -0,0 +1,20 @@
+class user():
+    """
+    """
+
+    def __init__(self, patient_id, email, age, heart_rate, time):
+        self.patient_id = patient_id
+        self.email = None
+        self.age = None
+        sgelf.heart_rate = None
+        self.time = None
+
+    def as_dict(self):
+         return {
+             "patient_id": self.patient_id,
+             "attending_email": self.email,
+             "user_age": self.age,
+             "heart_rate": self.heart_rate,
+             "heart_rate_average_since": self.time
+         }
+

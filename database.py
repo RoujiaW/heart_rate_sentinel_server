bc42b6af9474611840488c45ecacca9981dea2d8
RoujiaW <42785242+RoujiaW@users.noreply.github.com>
2018-11-15 13:05:01 -0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,13 @@
+from pymodm import connect
+from pymodm import MongoModel, fields
+
+
+connect("mongodb://vcm-7305.vm.duke.edu/heart_rate_server")
+
+
+class User(MongoModel):
+    user_id = fields.CharField(primary_key=True)
+    email = fields.EmailField()
+    age = fields.IntegerField()
+    heart_rate = fields.ListField(field=fields.IntegerField())
+    times = fields.ListField(field=fields.DateTimeField())

d931b1371e276ffb22bbc815b52fda8ac0024305
Roujia Wang <rosesharpaywang@RoujiamatoMacBook-Pro.local>
2018-11-15 16:19:42 -0500

    Establish server and database

--- database.py
+++ database.py
@@ -6,8 +6,9 @@ connect("mongodb://vcm-7305.vm.duke.edu/heart_rate_server")
 
 
 class User(MongoModel):
-    user_id = fields.CharField(primary_key=True)
+    user_id = fields.IntegerField(primary_key=True)
     email = fields.EmailField()
     age = fields.IntegerField()
     heart_rate = fields.ListField(field=fields.IntegerField())
     times = fields.ListField(field=fields.DateTimeField())
+    health = fields.CharField()
--- server.py
+++ server.py
@@ -1,15 +1,8 @@
 from flask import Flask, jsonify, request
 from disease import disease
 from database import User
-import datetime
+from datetime import datetime
 app = Flask(__name__)
-
-
-def add_heart_rate(patient_id, heart_rate, time):
-    user = User.objects.raw({"patient_id": patient_id}).first() # Get the first user where _id=email
-    user.heart_rate.append(heart_rate) # Append the heart_rate to the user's list of heart rates
-    user.heart_rate_times.append(time) # append the current time to the user's list of heart rate times
-    user.save() # save the user to the database
 
 
 @app.route("/api/new_patient", methods=["POST"])
@@ -20,63 +13,87 @@ def new_patient():
     r = request.get_json()
     user = User(r["patient_id"], r["attending_email"], r["user_age"])
     user.save()
-    # return jsonify(answer), 200
+    answer = {
+        "Message": "User create successfully",
+    }
+    return jsonify(answer), 200
 
 
-@app.route("/api/heart_rate",methods=["POST"])
+@app.route("/api/heart_rate", methods=["POST"])
 def heart_rate():
     """
-	post the new information about the heart rate measurement
-	and time
-	"""
+    post the new information about the heart rate measurement and time
+    """
     r = request.get_json()
     user = User.objects.raw({"user_id": r["patient_id"]}).first()
     user.heart_rate.append(r["heart_rate"])
-    user.times.append(datetime.datetime.now())
-    # return jsonify(answer), 200
-
-
-@app.route("/", methods=["GET"])
-def hello():
-    """
-    Returns the string "Hello, world" to the caller
-    """
-    return "Hello, world"
+    user.times.append(datetime.now())
+    result = disease(user.age, r["heart_rate"])  # most recent heart rate
+    if result:
+        user.health = "Patient has Tachycardia"
+    else:
+        user.health = "Patient's heart rate is normal"
+    answer = {
+        "Message": "User heart rate update successfully",
+    }
+    return jsonify(answer), 200
 
 
 @app.route("/api/status/<patient_id>", methods=["GET"])
-def hello(patient_id):
-
-    return "Hello, world"
-
-
-@app.route("/data", methods=["GET"])
-def getData():
+def status(patient_id):
     """
-    Returns the data dictionary below to the caller as JSON
+    :return: patient's health condition
     """
-    data = {
-        "name": "Suyash",
-        "team": "instructor"
+    user = User.objects.raw({"user_id": patient_id}).first()
+    answer = {
+        "health condition": user.health,
+        "Time": user.times[len(user.times) - 1],
     }
-    return jsonify(data)
+    return jsonify(answer), 200
 
 
-@app.route("/api/heart_rate/average/<patient_id>",methods=["GET"])
+@app.route("/api/heart_rate/<patient_id>", methods=["GET"])
+def heart_rate(patient_id):
+    """
+    :return: list of heart rate
+    """
+    user = User.objects.raw({"user_id": patient_id}).first()
+    result = {
+        "heart_rate_list": user.heart_rate
+    }
+    return jsonify(result), 200
+
+
+@app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
 def average(patient_id):
     """
-	return average for all measurements
-	"""
-# somehow grab the user and take the information from database
+    :return: average for all heart rate data
+    """
+    user = User.objects.raw({"user_id": patient_id}).first()
     average_heart_rate = sum(user.heart_rate)/len(user.heart_rate)
-    result = disease(user.age, average_heart_rate)
-    if result:
-		message = "Warning: the patient has Tachycardia"
-	else:
-		message = "Normal health condition"
-	answer = {
-		"average": average_heart_rate,
-		"health condition": message
-	}
-	return jsonify(answer),200
+    answer = {
+        "average": average_heart_rate,
+    }
+    return jsonify(answer), 200
 
+
+@app.route("/api/heart_rate/interval_average", methods=["POST"])
+def interval_average():
+    """
+    :return: interval_average based on user input
+    """
+    r = request.get_json()
+    user = User.objects.raw({"user_id": r["patient_id"]}).first()
+    input_time = datetime.strptime(r["heart_rate_average_since"],
+                                   '%Y-%m-%d %H:%M:%S.%f')
+    ave = None
+    for i in user.times:
+        if input_time >= i:
+            index = user.times(i)
+            end = len(user.heart_rate) - 1
+            heart_rate_interval = user.heart_rate[index: end]
+            ave = sum(heart_rate_interval)/len(heart_rate_interval)
+    if ave is None:
+        return {"Warning": "Input time is not existing"}
+    else:
+        return ave

1654e5c83095a842737b35b8033b34b17e692323
Roujia Wang <rosesharpaywang@RoujiamatoMacBook-Pro.local>
2018-11-15 12:59:57 -0500

    Update database

--- database.py
+++ database.py
@@ -11,18 +11,3 @@ class User(MongoModel):
     age = fields.IntegerField()
     heart_rate = fields.ListField(field=fields.IntegerField())
     times = fields.ListField(field=fields.DateTimeField())
-
-
-
-"""
-u = User('user1@email.com', last_name='Ross', first_name='Bob')
-u2 = User('user2@email.com', last_name='Ross', first_name='Rob')
-
-u.save()
-u2.save()
-
-for user in User.objects.raw({"first_name":"Rob"}):
-    print(user)
-	print(user.first_name)
-	print(user.last_name)
-"""

92b77da6e75da2b1d3d6d2c1dc303a660be19165
Roujia Wang <rosesharpaywang@RoujiamatoMacBook-Pro.local>
2018-11-17 10:23:41 -0500

    Compliant with pep 8

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
 
-
 r = requests.post("http://vcm-7305.vm.duke.edu:5000/api/new_patient",
-                   json={"patient_id": "1",
-                         "attending_email": "roujiw@uw.edu",
-                         "user_age": "50"})
+                  json={"patient_id": "1",
+                        "attending_email": "roujiw@uw.edu",
+                        "user_age": "50"})
 print(r.json())
 r = requests.post("http://vcm-7305.vm.duke.edu:5000/api/heart_rate",
                   json={"patient_id": "1", "heart_rate": 100})
@@ -21,8 +20,8 @@ r = requests.get("http://vcm-7305.vm.duke.edu:5000/api/heart_rate/1")
 print(r.json())
 r = requests.get("http://vcm-7305.vm.duke.edu:5000/api/heart_rate/average/1")
 print(r.json())
-r = requests.post("http://vcm-7305.vm.duke.edu:5000/api/heart_rate/interval_average",
-                  json={"patient_id": "1",
-                        "heart_rate_average_since":
-                            "2018-12-09 11:00:36.372339"})
+r = requests.post(
+    "http://vcm-7305.vm.duke.edu:5000/api/heart_rate/interval_average",
+    json={"patient_id": "1", "heart_rate_average_since":
+          "2018-12-09 11:00:36.372339"})
 print(r.json())
